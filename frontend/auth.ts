6575346ab7779094e887b09a8deb887272bac30f
TheSoftNode <thesoftnode@gmail.com>
2024-09-18 00:12:41 +0100

    Configure the google and github auth

--- frontend/auth.ts
+++ frontend/auth.ts
@@ -1,9 +1,11 @@
 import NextAuth from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import { prisma } from "./prisma/prisma"
+import Google from "next-auth/providers/google"
+import GitHub from "next-auth/providers/github"
 
- 
+
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [],
+  providers: [Google, GitHub],
 })
